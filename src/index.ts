63dc716659d502cdfe724cdfd3ec2da364262ef2
pullapavankumar <pullapavankumar.ca@gmail.com>
2024-03-11 20:23:35 -0400

    Removed static port

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,7 @@ FirebaseService.initialize()
 EmailConfig.initialize()
 MongoClient.initialize()
   .then(() => {
-    app.listen(3000, () => console.log("App is listening on port " + port))
+    app.listen(port, () => console.log("App is listening on port " + port))
   })
   .catch((error) => {
     console.error("Faile to start Server", error)
